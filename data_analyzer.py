109864909144309b1f35758cf7c04d011078cba6
Hung Thai <hung.thai@dwely.ai>
2026-05-06 11:30:52 +0700

    change

--- data_analyzer.py
+++ data_analyzer.py
@@ -5,10 +5,12 @@ class DataAnalyzer:
     def __init__(self, file_path: str):
         self.file_path = file_path
         self.df = None
+        
     def load_data(self):
         self.df = pd.read_csv(self.file_path)
         print(f"Loaded. Data have {len(self.df)} rows and {len(self.df.columns)} columns")
         print(self.df.dtypes)
+        
     def cleaning_data(self):
         print("=== Missing Values (before) ===")
         print(self.df.isnull().sum(), "\n")
@@ -19,14 +21,26 @@ class DataAnalyzer:
                 self.df[column].dropna()
         print("=== Missing Values (after) ===")
         print(self.df.isnull().sum(), "\n")
-    def compute_staus(self, column):
+        
+    def compute_status(self, column):
         arr = self.df[column].dropna().to_numpy()
-        print(f"Mean: {np.mean(arr)}")
-        print(f"Median: {np.median(arr)}")
-        print(f"Standard Deviation: {np.std(arr)}")
-        print(f"Max: {np.max(arr)}")
-        print(f"Min: {np.min(arr)}")
-
+        status = {
+            "mean" : np.mean(arr),
+            "median" : np.median(arr),
+            "std" : np.std(arr),
+            "max" : np.max(arr),
+            "min" : np.min(arr),
+            "q25" : np.percentile(arr, 25),
+            "q75" : np.percentile(arr, 75)
+        }
+        print(f"Mean: {status["mean"]}")
+        print(f"Median: {status["media"]}")
+        print(f"Standard Deviation: {status["std"]}")
+        print(f"Max: {status["max"]}")
+        print(f"Min: {status["min"]}")
+        print(f"Q25: {status["q25"]}, Q75: {status["q75"]}")
+        
+        return status
         
     
         
